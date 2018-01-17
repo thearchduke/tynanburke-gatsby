0fe61ea5ea65f11a7dbcd36a8d8652ed43f706fd
Tynan Burke <burke@carnegiehub.org>
2018-01-16 16:49:20 -0800

    waypoint commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,10 +4,19 @@ module.exports = {
     },
     plugins: [
         'gatsby-plugin-react-helmet',
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
         {
             resolve: `gatsby-plugin-typography`,
             options: {
                 pathToConfigModule: `src/utils/typography`,
+            },
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `img`,
+                path: `${__dirname}/src/images/`
             },
         },
     ],
