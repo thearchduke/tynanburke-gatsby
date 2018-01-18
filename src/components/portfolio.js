75eeceacccd9a4266d1ffb87070d4560e438cce0
Tynan Burke <burke@carnegiehub.org>
2018-01-18 09:56:18 -0800

    README, portfolio component

---
+++ src/components/portfolio.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+
+const PortfolioItem = props => (
+    <li>
+        <h4 id={props.headline}><a name={props.headline}></a>{props.headline}</h4>
+        {props.children}
+    </li>
+)
+
+export default () => (
+<div>
+    <h2>Portfolio</h2>
+    <ul>
+        <PortfolioItem headline="Gallery">
+            <p>(pictures of visualizations and such)</p>
+        </PortfolioItem>
+        <PortfolioItem headline="Pathfork">
+            <p><a href="https://pathfork.herokuapp.com">Pathfork</a> is a website that I built because I wanted a cloud-based writing organization tool. It lets you compose stories, and manage information about your characters and settings, adding them like blog tags to the sections of your work. I use it to organize short stories and my webcomic, though I saw the initial need while trying to remember which characters in a novel wore watches.</p>
+            <p>It is written in Go, Postgres, and javascript, and served by Heroku, and the code is available <a href="https://github.com/thearchduke/pathfork">on GitHub</a>.</p>
+        </PortfolioItem>
+        <PortfolioItem headline="Wordpress picture tool">
+            <p><a href="https://github.com/thearchduke/wordpress-picture-submissions">A Flask app</a> that takes submissions for a travel photos feature, provides an admin panel for managing them, and ultimately posts the collections you select as draft posts to a Wordpress site. Currently live.</p>
+        </PortfolioItem>
+        <PortfolioItem headline="other">
+            <p>This site (tynanburke.com) was made in <a href="https://www.gatsbyjs.org">gatsby</a>, a react-based static site generator. The code is <a href="https://github.com/thearchduke/tynanburke-gatsby">on GitHub</a>.</p>
+        </PortfolioItem>
+    </ul>
+</div>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Link from 'gatsby-link'
 import UrlFor from '../utils/urls'
 
+
 export default () => (
   <div>
     <h1>Greetings.</h1>
@@ -10,9 +11,10 @@ export default () => (
     <p>According to the logs, most people come here for my <Link to={UrlFor("work")}>resumé and portfolio</Link>.</p>
 
     <p>If you are not one of these people, or if you are but would like to see other things as well, 
-I also write and draw <Link to="/words#againststupidity">a webcomic</Link>, 
-host <Link to={UrlFor("work") + '#pathfork'}>a free writing-tools website</Link>, 
-(very) occasionally <a href="https://tynan.blog">blog</a>,
-and have <Link to={UrlFor("photography")}>a small sampling</Link> of photography.</p>
+        I also write and draw <Link to="/words#againststupidity">a webcomic</Link>, 
+        host <Link to={UrlFor("work") + '#Pathfork'}>a free writing-tools website</Link>, 
+        (very) occasionally <a href="https://tynan.blog">blog</a>,
+        and have <Link to={UrlFor("photography")}>a small sampling</Link> of photography.
+    </p>
   </div>
 )
