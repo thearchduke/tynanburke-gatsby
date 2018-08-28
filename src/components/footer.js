15e46974e59d669400351a65ac6159b5af02e819
Tynan Burke <burke@carnegiehub.org>
2018-08-28 11:12:05 -0700

    Removed comment on footer (build bug fixing attempt)

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,15 +6,6 @@ import gatsbyLogo from '../images/gatsby.svg'
 export default () => (
     <div style={{ margin: `0 auto`, width: `80%`, fontSize: `75%`, textAlign: `center`, verticalAlign: `middle` }}>
         <hr/>
-        {/*
-        <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/" style={{ textDecoration: `none`, textShadow: `none`, backgroundImage: `none` }}>
-            <img
-                alt="Creative Commons License" 
-                src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" 
-                style={{ margin: `0 auto`, display: `block` }}
-            />
-        </a>
-        This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.*/}
         <a href="https://gatsbyjs.org" style={{ textDecoration: `none`, textShadow: `none`, backgroundImage: `none` }}>
             <img 
                 src={gatsbyLogo} 
