145b5b176e28fd06f7289c8486874fb58a0eb972
tynan <tynanburke@gmail.com>
2020-01-25 12:03:36 -0500

    update writing

--- src/pages/writing.js
+++ src/pages/writing.js
@@ -17,7 +17,7 @@ const WritingInner = () => (
             maxWidth: "360px",
             width: "20%",
             float: "right",
-            marginLeft: "1rem",
+            marginLeft: "1rem"
           }}
         >
           <a href="https://books.metaphorosis.com/anthology/2019/metaphorosis-best-of-2018/">
@@ -38,17 +38,43 @@ const WritingInner = () => (
         <br />
         <br />
         <blockquote>
-          Yonatan Kaplan runs a curious clipping service in the Lower East Side,
-          working hard to stave off armageddon. When a troubling new customer
-          calls, and one of his prophets comes under scrutiny, he's forced to
-          grapple with more than he's used to on a Friday night.
+          Last night, Murray called with another bunch of prophecies, so Yonatan
+          Kaplan hasn't slept yet. He stayed up preparing dossiers on some
+          doomed socialites instead. Now it's a little after dawn, Friday
+          morning, and he's standing in line outside Fox's Bagels with a thermos
+          and a tote bag. He's shaky from too much caffeine and too little
+          sleep, but he doesn't regret it. The socialites will die this weekend,
+          according to Murray, and Murray’s got a good track record...
+        </blockquote>
+      </li>
+      <li>
+        I'm working on the <em>Floodland Cycle</em>, a fantasy short story cycle
+        set in a world with curious hydrology. The first of these stories is
+        available in the January 2019 issue of{" "}
+        <em>
+          <a href="http://www.swordsandsorcerymagazine.com/index.html">
+            Swords and Sorcery Magazine
+          </a>
+        </em>
+        .
+        <br />
+        <br />
+        <blockquote>
+          There are monks in the far west who you may have heard of, called
+          Harvesters. They teach that every day changes your life forever. Of
+          course, some days change it more than others; this is about one of
+          <em>those</em> days. I was sixteen, a good age for it.
+          <br />
+          First give me a moment to tell about my ancestral home: Samyra, the
+          Jewel of the South, the City of Palaces. This rain-drenched citadel is
+          built on top of a crag that sticks up from the middle of the coastal
+          Samyran Basin. During flood season—most of the year—the Basin fills to
+          become a tidal estuary...
         </blockquote>
       </li>
       <li>
         I'll have five short stories in the <em>Reading 5X5 x2</em> anthology,
-        which should come out in time for holiday shopping season. I'll update
-        this when it's available (and I'm sure I'll be tweeting about it
-        excessively too).
+        which should come out this summer. Check back for updates!
       </li>
     </ul>
 
@@ -60,7 +86,7 @@ const WritingInner = () => (
             maxWidth: "500px",
             width: "30%",
             float: "right",
-            marginLeft: "1rem",
+            marginLeft: "1rem"
           }}
         >
           <Samwise />
